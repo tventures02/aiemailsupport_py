857b9d61be70ac186cc86dd5b10a3279601d7211
jinyu112 <jinyuguan1102@gmail.com>
2023-08-29 09:49:01 -0400

    add requirements.txt for dependencies

--- email_support_QA_prompts.py
+++ email_support_QA_prompts.py
@@ -14,7 +14,7 @@ EMAIL_SUPPORT_QA_SYSTEM_PROMPT = ChatMessage(
         "2. Avoid statements like 'Based on the context, ...' or "
         "'The context information ...' or anything along those lines.\n"
         "3. Never mention that you are an email support system or an AI!\n"
-        "4. Never make up an answer.\n"
+        "4. Never make up an answer. If you are unsure, ask the customer to clarify about whatever they're asking for.\n"
         "5. Avoid statments like 'send us an email' or 'send an email' or anything along those lines.\n\n"
         "If you cannot form an exact response from the contextual information, "
         # "if there is large uncertainty in forming the answer or "
