63811a0f0b445c0748e4400e1b2c10247bbbe3d9
EC2 Default User <ec2-user@ip-172-31-4-80.ec2.internal>
2023-10-31 20:38:00 +0000

    get user document from mongodb atlas cluster

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
@@ -20,17 +21,46 @@ def call_support_scribe():
 @app.route('/createAndSaveDocumentIndex', methods=['POST'])
 def create_and_save_document_index():
     from aiemailsupport_vectorstore.createAndSaveIndex import main
-    data = request.json
-    docId = data.get('docId', '')
-    collection = data.get('collection', '')
-    refreshToken = data.get('refreshToken', '')
-    output = main( collection, docId, refreshToken)
-    success = output["success"]
-
-    if success:
-        return jsonify(success=True, message=output["message"]), 200
-    else:
-        return jsonify(success=False,error=output["error"]), 500
+    
+    try:
+        data = request.json
+        email = data.get('email', '')
+        if email == '':
+            raise ValueError("No email found.")
+        
+        MONGO_URI='mongodb+srv://bpadmin:IMBF9iORG3TVS8XP@bp-dev-and-local.guo9v5t.mongodb.net/supportscribe?retryWrites=true&w=majority'
+        client = MongoClient(MONGO_URI, tlsAllowInvalidCertificates=True)
+        mongodb = client['supportscribe']
+        mongodbCollection = mongodb['users']
+        
+        
+        user = mongodbCollection.find_one({"email": email})
+        
+        
+        documents = user['documents']
+        googleTokens = user['googleTokens']
+        
+        
+        doc = documents[0]
+        docId = doc['id']
+        collection = user['email']
+        refreshToken = googleTokens['refreshToken']
+        
+        print(docId)
+        print(collection)
+        print(refreshToken)
+        return jsonify(success=True), 200
+        
+        # output = main( collection, docId, refreshToken)
+        # success = output["success"]
+    
+        # if success:
+        #     return jsonify(success=True, message=output["message"]), 200
+        # else:
+        #     return jsonify(success=False,error=output["error"]), 500
+    except Exception as e:
+        print(e)
+        return jsonify(success=False,error=str(e)), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)  # Running on port 5000
